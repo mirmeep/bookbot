0b213975aa74b2502badab841d4ef2cb8b642373
mirmeep <mirpeak@gmail.com>
2024-12-29 11:27:08 -0800

    Add function to count occurence of characters in text.

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = count_words(text)
-    print(num_words)
+    chars_in_text = count_char(text)
+    print(chars_in_text)
 
 def get_book_text(path):
     with open(path) as f:
@@ -11,5 +12,15 @@ def get_book_text(path):
 def count_words(text):
     words = text.split()
     return len(words)
+
+def count_char(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
 
 main()
