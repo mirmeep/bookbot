c7fdd5913ae066231470b9a3b0bdbf652639f7ed
mirmeep <mirpeak@gmail.com>
2024-12-29 11:51:50 -0800

    Add a report with sorted occurences of characters in text.

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ def main():
     text = get_book_text(book_path)
     num_words = count_words(text)
     chars_in_text = count_char(text)
-    print(chars_in_text)
+    chars_sorted_list = sort_dict(chars_in_text)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    for item in chars_sorted_list:
+        if item["char"].isalpha():
+            print(f"The '{item['char']}' character was found {item['num']} times")
+    print("--- End Report ---")
 
 def get_book_text(path):
     with open(path) as f:
@@ -12,6 +19,16 @@ def get_book_text(path):
 def count_words(text):
     words = text.split()
     return len(words)
+
+def sort_on(d):
+    return d["num"]
+
+def sort_dict(chars_in_text):
+    sorted_list = []
+    for c in chars_in_text:
+        sorted_list.append({"char": c, "num": chars_in_text[c]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
 
 def count_char(text):
     chars = {}
